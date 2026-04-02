9d9ae100617bc88b7d42c907d46509955b39d559
Aradhna Jaiswal <aradhanajaiswal294@gmail.com>
2026-04-02 15:06:15 +0530

    string formatting

---
+++ chapter10/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+W="Welcome {} to {} Wscubetech".format("hello",20);
+print(W)
+
+W="Welcome {1} to {0} Wscubetech".format("hello",20);
+print(W)
+
+W="Welcome {b} to {a} Wscubetech".format(a=30,b=40)
+print(W)
